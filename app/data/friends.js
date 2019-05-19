4c07a60313e40cb6254c98eca4eab0d5c92210b4
KevinL <mastahkevin@gmail.com>
2019-05-19 16:09:42 -0700

    Redesigned front end and theme. Added friends for friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+const friends = [
+    {
+        name: "Bizarro",
+        photo: "assets/images/bizarro.jpg",
+        scores: [
+            "1",
+            "5",
+            "1",
+            "3",
+            "5",
+            "3",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        name: "Dr. Doom",
+        photo: "assets/images/drDoom.jpg",
+        scores: [
+            "4",
+            "2",
+            "3",
+            "2",
+            "3",
+            "2",
+            "2",
+            "5",
+            "4",
+            "3"
+        ]
+    },
+    {
+        name: "Joker",
+        photo: "assets/images/joker.jpg",
+        scores: [
+            "2",
+            "3",
+            "5",
+            "5",
+            "1",
+            "4",
+            "3",
+            "2",
+            "5",
+            "4"
+        ]
+    },
+    {
+        name: "Lex Luthor",
+        photo: "assets/images/lexluthor.jpg",
+        scores: [
+            "5",
+            "4",
+            "2",
+            "1",
+            "4",
+            "5",
+            "5",
+            "3",
+            "3",
+            "5"
+        ]
+    },
+    {
+        name: "Magneto",
+        photo: "assets/images/magneto.jpg",
+        scores: [
+            "4",
+            "1",
+            "4",
+            "4",
+            "2",
+            "1",
+            "4",
+            "4",
+            "2",
+            "2"
+        ]
+    }
+]
+
+module.exports = friends;
