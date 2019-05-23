cf5677bab8219c46f97dee1ceedd0fbd4e694db8
KevinL <mastahkevin@gmail.com>
2019-05-23 14:59:48 -0700

    Merging changes.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,24 +5,16 @@ const friends = require('../data/friends');
 //=================================== //
 
 router.get('/api/friends', function (req, res) {
-<<<<<<< HEAD
     res.json(friends);
     // console.log(friends);
 });
 
 router.post('/api/friends', function (req, res) {
     // use best match object to hold all options as we loop through 
-=======
-    return res.json(friends);
-});
-
-router.post('/api/friends', function (req, res) {
->>>>>>> 6d9f862404e50ee89ee758b71d41100c7c4931d6
     let bestMatch = {
         name: '',
         photo: '',
         friendDifference: ''
-<<<<<<< HEAD
     };
 
     // take result of user's survey, POST and parse
@@ -45,41 +37,17 @@ router.post('/api/friends', function (req, res) {
             if (totalDifference <= bestMatch.friendDifference) {
                 
                 // reset bestMatch to be new friend.
-=======
-    }
-
-    let userData = req.body;
-    let userScores = userData.scores;
-
-    let totalDifference = 0;
-
-    for (let i = 0; i < friends.length; i++) {
-        totalDifference = 0;
-
-        for (let j = 0; j < friends[i].scores[j]; j++) {
-            totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
-
-            //If sum of differences is less then the differences of the current match
-            if (totalDifference <= bestMatch.friendDifference) {
-
-                //Reset the bestMatch to be the new friend.
->>>>>>> 6d9f862404e50ee89ee758b71d41100c7c4931d6
                 bestMatch.name = friends[i].name;
                 bestMatch.photo = friends[i].photo;
                 bestMatch.friendDifference = totalDifference;
             }
         }
     }
-<<<<<<< HEAD
 
     // save the users' data to the db 
     friends.push(userData);
 
     // Return a JSON with the users best match
-=======
-    friends.push(userData);
-
->>>>>>> 6d9f862404e50ee89ee758b71d41100c7c4931d6
     res.json(bestMatch);
 });
 
