3ff70b12ab864276b9bbbdca35b02140ab15604c
KevinL <mastahkevin@gmail.com>
2019-05-23 23:10:51 -0700

    Updated friends.js to use url instead of static files & survey.js to serve up the modal.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 const friends = [
     {
         name: "Bizarro",
-        photo: "assets/images/bizarro.jpg",
+        photo: "https://vignette.wikia.nocookie.net/injusticegodsamongus/images/5/55/Injustice2-BIZARRO-wallpaper-mobile-83.jpg/revision/latest?cb=20170916181948",
         scores: [1,5,1,3,5,3,1,1,1,1]
     },
     {
         name: "Dr. Doom",
-        photo: "assets/images/drDoom.jpg",
+        photo: "https://upload.wikimedia.org/wikipedia/en/6/60/Doctor_Doom_%282018%29.jpg",
         scores: [4,2,3,2,3,2,2,5,4,3]
     },
     {
         name: "Joker",
-        photo: "assets/images/joker.jpg",
+        photo: "https://i.pinimg.com/736x/14/d1/c0/14d1c0fd755f10391c7b4fa62fccf754.jpg",
         scores: [2,3,5,5,1,4,3,2,5,4]
     },
     {
         name: "Lex Luthor",
-        photo: "assets/images/lexluthor.jpg",
+        photo: "https://upload.wikimedia.org/wikipedia/en/d/d5/LexLuthor1.png",
         scores: [5,4,2,1,4,5,5,3,3,5]
     },
     {
         name: "Magneto",
-        photo: "assets/images/magneto.jpg",
+        photo: "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/ApplicationPageBlock/60613/5ade3133098a6c5f33c37241c6a9a0f021fc2d29._QL80_TTD_.jpg",
         scores: [4,1,4,4,2,1,4,4,2,2]
     }
 ]
